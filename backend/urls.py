0cfab296a39d15b6a2200b093c369fd767479546
bowingman <rabbit@sky.io>
2022-11-16 06:52:08 -0500

    current

--- backend/urls.py
+++ backend/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
+    path('', include('frontend.urls')),
 ]
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Route, BrowserRouter, Routes} from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="flex h-screen text-xl md:text-lg">
+    <BrowserRouter>
+    <div>
+    
+    <Routes>
+
+      <Route exact path="/login" element={<div><div className="flex h-screen text-xl md:text-lg">
       <div className="m-auto text-center">
       Made with ❤️ using 
     <a href="https://docs.djangoproject.com/en/3.2/" className="font-bold text-django" target="_blank"> Django</a>,
         <a href="https://reactjs.org/docs/getting-started.html" className="font-bold text-react" target="_blank"> React</a> and 
         <a href="https://tailwindcss.com/docs" className="font-bold text-tailwind" target="_blank"> Tailwind CSS</a>
       </div>
+    </div></div>} /> 
+      <Route exact path="/register" element={<div>456</div>} /> 
+    </Routes>
     </div>
+    </BrowserRouter>
   );
 };
 
