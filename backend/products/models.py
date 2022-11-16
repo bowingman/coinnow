5d823b654dd696f2e4d823f5541ea4dd4c53fb3c
bowingman <rabbit@sky.io>
2022-11-16 04:27:11 -0500

    Added product model & category model

---
+++ backend/products/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from backend.categories.models import Category
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    description = models.TextField()
+    image = models.TextField()
+    current_price = models.IntegerField()
+    minimum_price = models.IntegerField()
+    maximum_price = models.IntegerField()
+    quantity = models.IntegerField()
+    auto_stock_amount = models.IntegerField()
+    price_change_amount = models.IntegerField()
--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'frontend'
+    'frontend',
+    'backend.categories',
+    'backend.products',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'coinnow',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
