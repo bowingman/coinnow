7e8629d5e5278ae78d4021591720c98c13d3f81c
bowingman <ollielucke@gmail.com>
2022-11-16 14:07:43 -0500

    Added static product list page

--- frontend/src/components/layouts/sidebar.js
+++ frontend/src/components/layouts/sidebar.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div className='bg-black'>
-
+    <div className='w-[340px] h-[100vh] bg-[#0C0C0C] rounded-[13px]'>
+      <div>
+        <div className='text-[30px] text-white pt-[77px] ml-[37px]'>{"Coinnow Tech"}</div>
+      </div>
+      <div className='pt-[170px] ml-[37px] text-[30px] text-white'>
+        <div className='cursor-pointer'>{">Products"}</div>
+        <div className='mt-8 cursor-pointer'>{">Users"}</div>
+      </div>
     </div>
   )
 }
--- frontend/src/containers/products/index.js
+++ frontend/src/containers/products/index.js
@@ -1,9 +1,113 @@
 import React from "react"
 
+import SideBar from "../../components/layouts/sidebar"
+
 const ProductList = () => {
   return (
-    <div className="bg-black">
-      <h1>Product List Page</h1>
+    <div className="bg-[#292B2E] h-screen w-screen flex">
+      <SideBar />
+      <div className="mt-[77px] ml-[30px] text-[32px] text-white">
+        <div className="w-[1387px] h-[140px] bg-[#0C0C0C] rounded-[13px] flex justify-center items-center">
+          <div>Search Product By Name</div>
+        </div>
+        <div className="mt-[44px]">
+          <div className="w-[1387px] h-[110px] bg-[#222222] rounded-[13px] flex items-center">
+            <div className="flex w-full">
+              <div className="w-1/4 flex items-center">
+                <div className="ml-[76px]">Product Name</div>
+              </div>
+              <div className="w-1/4 flex items-center">
+                <div className="ml-[76px]">Available Quantity</div>
+              </div>
+              <div className="w-1/3 flex items-center">
+                <div className="ml-[76px]">Total Owned By Users</div>
+              </div>
+              <div className="w-1/6 flex items-center">
+                <div className="ml-[30px]">Current Price</div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-[22px]">
+            <div className="mb-[25px] flex">
+              <div className="w-[1387px] h-[110px] bg-[#222222] rounded-[13px] flex items-center">
+                <div className="flex w-full">
+                  <div className="w-1/4 flex items-center">
+                    <div className="ml-[76px]">Product1</div>
+                  </div>
+                  <div className="w-1/4 flex items-center">
+                    <div className="ml-[76px]">400</div>
+                  </div>
+                  <div className="w-1/3 flex items-center">
+                    <div className="ml-[76px]">100</div>
+                  </div>
+                  <div className="w-1/6 flex items-center">
+                    <div className="ml-[30px]">2000</div>
+                  </div>
+                </div>
+              </div>
+              <button className="bg-[#0047FF] w-[110px] h-[110px] rounded-[13px] ml-[26px]">Edit</button>
+            </div>
+            <div className="mb-[25px] flex">
+              <div className="w-[1387px] h-[110px] bg-[#222222] rounded-[13px] flex items-center">
+                <div className="flex w-full">
+                  <div className="w-1/4 flex items-center">
+                    <div className="ml-[76px]">Product1</div>
+                  </div>
+                  <div className="w-1/4 flex items-center">
+                    <div className="ml-[76px]">400</div>
+                  </div>
+                  <div className="w-1/3 flex items-center">
+                    <div className="ml-[76px]">100</div>
+                  </div>
+                  <div className="w-1/6 flex items-center">
+                    <div className="ml-[30px]">2000</div>
+                  </div>
+                </div>
+              </div>
+              <button className="bg-[#0047FF] w-[110px] h-[110px] rounded-[13px] ml-[26px]">Edit</button>
+            </div>
+            <div className="mb-[25px] flex">
+              <div className="w-[1387px] h-[110px] bg-[#222222] rounded-[13px] flex items-center">
+                <div className="flex w-full">
+                  <div className="w-1/4 flex items-center">
+                    <div className="ml-[76px]">Product1</div>
+                  </div>
+                  <div className="w-1/4 flex items-center">
+                    <div className="ml-[76px]">400</div>
+                  </div>
+                  <div className="w-1/3 flex items-center">
+                    <div className="ml-[76px]">100</div>
+                  </div>
+                  <div className="w-1/6 flex items-center">
+                    <div className="ml-[30px]">2000</div>
+                  </div>
+                </div>
+              </div>
+              <button className="bg-[#0047FF] w-[110px] h-[110px] rounded-[13px] ml-[26px]">Edit</button>
+            </div>
+            <div className="mb-[25px] flex">
+              <div className="w-[1387px] h-[110px] bg-[#222222] rounded-[13px] flex items-center">
+                <div className="flex w-full">
+                  <div className="w-1/4 flex items-center">
+                    <div className="ml-[76px]">Product1</div>
+                  </div>
+                  <div className="w-1/4 flex items-center">
+                    <div className="ml-[76px]">400</div>
+                  </div>
+                  <div className="w-1/3 flex items-center">
+                    <div className="ml-[76px]">100</div>
+                  </div>
+                  <div className="w-1/6 flex items-center">
+                    <div className="ml-[30px]">2000</div>
+                  </div>
+                </div>
+              </div>
+              <button className="bg-[#0047FF] w-[110px] h-[110px] rounded-[13px] ml-[26px]">Edit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
