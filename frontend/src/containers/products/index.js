a5a6248a500732ce902b5969a07e72e577dae54e
bowingman <ollielucke@gmail.com>
2022-11-16 07:39:26 -0500

    current position

---
+++ frontend/src/containers/products/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const ProductList = () => {
+  return (
+    <div className="bg-black">
+      <h1>Product List Page</h1>
+    </div>
+  )
+}
+
+export default ProductList
